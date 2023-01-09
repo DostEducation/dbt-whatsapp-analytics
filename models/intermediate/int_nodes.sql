38a2410b68f481fbc4e42f1ae5120b0f6cfeadf7
Swapneel Rane <swapneel@goalkeep.net>
2023-01-09 13:18:09 +0000

    add int_nodes

---
+++ models/intermediate/int_nodes.sql
@@ -0,0 +1,33 @@
+with
+    flows as (select * from {{ ref("stg_flows") }}),
+
+    extract_nodes_array as (
+        -- name as flow_name,
+        -- status as flow_status,
+        -- inserted_at as flow_created_on,
+        -- updated_at as flow_updated_on,
+        select flow_uuid, json_query_array(flow_config_json, "$.nodes") as node,
+        from flows
+    ),
+
+    unnest_nodes_array as (
+        select
+            json_value(node, "$.uuid") as node_uuid,
+            extract_nodes_array.flow_uuid,
+            node_sequence,
+            if(
+                array_length(json_query_array(node, "$.actions")) = 0,
+                json_value(node, "$.router.type"),
+                json_value(node, "$.actions[0].type")
+            ) as node_type_n1,
+            json_value(node, "$.actions[0].text") as message_text_n1,
+            json_value(node, "$.actions[0].groups[0].name") as group_name_n1,
+            json_value(node, "$.actions[0].value") as field_value_n1,
+            json_query(node, "$") as node_config
+        from extract_nodes_array, unnest(extract_nodes_array.node) node
+        with
+        offset as node_sequence
+    )
+
+select *
+from unnest_nodes_array
