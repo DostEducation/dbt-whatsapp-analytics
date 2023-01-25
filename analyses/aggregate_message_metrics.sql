cfffd7868888bbf4192989c5e982164a1177b800
Swapneel Rane <swapneel@goalkeep.net>
2023-01-25 03:48:11 +0000

    calculate % users responding to each bot feature

--- analyses/aggregate_message_metrics.sql
+++ analyses/aggregate_message_metrics.sql
@@ -1,29 +1,32 @@
 with
-    messages as (select * from {{ source("glific", "messages") }}),
-
-    get_latest_message_record as (
-        select *
-        from
-            (
-                select
-                    row_number() over (
-                        partition by id order by bq_inserted_at desc
-                    ) as row_number,
-                    messages.*
-                from messages
-            )
-        where row_number < 2
-    ),
+    messages as (select * from {{ ref("stg_messages") }}),
 
     message_count_by_direction_type_status as (
         select
-            flow,
             type,
             status,
-            count(id)
-        from get_latest_message_record
-        group by 1, 2, 3
-        order by 1, 2, 3
+            -- count(message_id) as count_of_messages,
+            count(distinct contact_phone) as count_of_users,
+        from messages
+        where status <> 'enqueued'
+        group by 1, 2
+        order by 1, 2
+    ),
+
+    pivot_by_status as (
+        select *
+        from
+            message_count_by_direction_type_status pivot (
+                sum(count_of_users) for status in ('sent', 'received')
+            ) as pivot_results
+    ),
+
+    percent_users_responding as (
+        select
+            *,
+            if(received < sent, safe_divide(received, sent), 1) as percent_users_responding
+        from pivot_by_status
     )
 
-select * from message_count_by_direction_type_status
+select *
+from percent_users_responding
