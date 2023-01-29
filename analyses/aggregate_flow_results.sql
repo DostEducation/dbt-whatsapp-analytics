ee8f407f2c43f95d0e62c05b5884342a6093c4a0
Swapneel Rane <swapneel@goalkeep.net>
2023-01-29 17:21:53 +0000

    change sorting order for aggregated flow results

--- analyses/aggregate_flow_results.sql
+++ analyses/aggregate_flow_results.sql
@@ -4,7 +4,7 @@ with
     aggregate_responses_by_result_key as (
         select
             result_key,
-            count(distinct contact_phone) as count_of_contacts
+            count(distinct contact_phone) as total_responses
         from flow_results
         group by 1
     ),
@@ -16,5 +16,17 @@ with
             count(distinct contact_phone) as count_of_contacts
         from flow_results
         group by 1, 2
-        order by 1, 2
     )
+
+select
+    result_key,
+    result_category,
+    total_responses,
+    count_of_contacts,
+    safe_divide(count_of_contacts, total_responses) as response_rate
+from
+    aggregate_responses_by_result_key 
+    left join aggregate_responses_by_result_key_and_category using (result_key)
+order by
+    3 desc, 1, 5 desc
+
