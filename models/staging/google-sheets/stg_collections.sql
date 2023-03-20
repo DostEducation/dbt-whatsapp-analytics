19bb016d58832a49c7bd65b24b1d207b5ffc8469
swapneel@goalkeep.net <swapneel@goalkeep.net>
2023-03-20 07:19:22 +0000

    fix column names on google sheet staging tables for collections and node_cnfigs

--- models/staging/google-sheets/stg_collections.sql
+++ models/staging/google-sheets/stg_collections.sql
@@ -1,25 +1,24 @@
+with
+    source as (
 
+        select * from {{ source("whatsapp_analytics_config", "src_collections") }}
 
-with source as (
+    ),
 
-    select * from {{ source('whatsapp_analytics_config', 'src_collections') }}
+    renamed as (
 
-),
+        select
+            id,
+            name,
+            description,
+            _airbyte_ab_id,
+            _airbyte_emitted_at,
+            _airbyte_normalized_at,
+            _airbyte_src_collections_hashid
 
-renamed as (
+        from source
 
-    select
-        id,
-        name,
-        flow_name,
-        description,
-        _airbyte_ab_id,
-        _airbyte_emitted_at,
-        _airbyte_normalized_at,
-        _airbyte_src_collections_hashid
+    )
 
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
