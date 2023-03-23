700ed32e85cd5b7ef01627f8498f8e52c03527e1
swapneel@goalkeep.net <swapneel@goalkeep.net>
2023-03-23 09:27:04 +0000

    removed flow_id filter on int_nodes

--- models/intermediate/int_nodes.sql
+++ models/intermediate/int_nodes.sql
@@ -29,11 +29,4 @@ with
 
 select *
 from unnest_nodes
-where
-    flow_uuid in (
-        'bf2e5555-689f-4708-b9b5-cc6bab8ecf70', -- activation flow
-        '094ed199-1b6c-42a6-80bb-f46617fbb937', -- aws registration 1.0
-        '9c797785-3062-4295-a824-c3237ecbc98a', -- aws registration 1.1
-        'a4900527-f7bc-4dd7-afea-32803280cde1' -- aws registration 1.2
-    )
 
