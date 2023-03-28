d06a49cc641ed1d34222d019bf168d786848c913
swapneel@goalkeep.net <swapneel@goalkeep.net>
2023-03-28 05:17:38 +0000

    add sector_from_glific to fct_contacts

--- models/intermediate/int_contacts.sql
+++ models/intermediate/int_contacts.sql
@@ -9,6 +9,10 @@ with
         select * from {{ ref("int_contact_fields") }} where contact_field = 'user type'
     ),
 
+    sector_on_glific as (
+        select * from {{ ref("int_contact_fields") }} where contact_field = 'sector'
+    ),
+
     append_aws_and_aww_contacts_expected as (
         select *
         from aws_contacts_expected
@@ -32,15 +36,17 @@ with
         full outer join glific_contacts using (contact_phone)
     ),
 
-    add_user_type as (
+    add_contact_fields as (
         select
             join_expected_and_actual_contacts.*,
             upper(user_type.contact_field_value) as user_type_from_glific,
+            (sector_on_glific.contact_field_value) as sector_from_glific,
             user_type.contact_field_inserted_at
         from join_expected_and_actual_contacts
-        left join user_type using (contact_id)
+            left join user_type using (contact_id)
+            left join sector_on_glific using (contact_id)
     )
 
 select *
-from add_user_type
+from add_contact_fields
 -- where contact_phone is not null 
