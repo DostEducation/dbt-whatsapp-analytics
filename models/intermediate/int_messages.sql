78378708d7045db5af6a7fc1bae674d7010c3517
swapneel@goalkeep.net <swapneel@goalkeep.net>
2023-04-18 12:04:09 +0000

    for int_messages, switched source to int_nodes to add additional flow and node info

--- models/intermediate/int_messages.sql
+++ models/intermediate/int_messages.sql
@@ -1,12 +1,12 @@
 with
     messages as (select * from {{ ref('stg_messages') }}),
 
-    node_configs as (select * from {{ ref('stg_node_configs') }}),
+    node_configs as (select * from {{ ref('int_nodes') }}),
 
     add_node_info_to_messages as (
         select
             messages.*,
-            node_configs.* except (node_label, flow_name, flow_uuid)
+            node_configs.* except (node_label, flow_name, flow_uuid, flow_id, updated_at)
         from messages
             left join node_configs using (node_label)
     )
