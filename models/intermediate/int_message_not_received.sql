5fae0322f437480367cabe05c6c3d3464db13b39
Vatsal Gosalia <vatsalgosalia8@gmail.com>
2023-07-31 17:08:31 +0530

    flows that have not sent a single outbound message

--- models/intermediate/int_message_not_received.sql
+++ models/intermediate/int_message_not_received.sql
@@ -3,9 +3,8 @@ with
     contacts as (select * from {{ ref('fct_contacts') }}),
     add_row_number as (
         select
-            flow_name,
-            message_inserted_at,
-            row_number() over (partition by flow_name order by message_inserted_at desc) as row_number,
+            *,
+            row_number() over (partition by contact_phone order by message_inserted_at desc) as row_number,
         from messages
         {# where message_inserted_at >= current_datetime() - interval 7 day #}
     ),
@@ -15,9 +14,14 @@ with
         from add_row_number
         where row_number = 1
     ),
+
     get_latest_flow_uuid as (
         select
+            contact_phone,
+            flow_uuid as latest_flow_uuid,
             flow_name as latest_flow_name,
+            message_direction as latest_message_direction,
+            message_status as latest_message_status,
             message_inserted_at as latest_message_inserted_at
         from
             get_latest_message_for_contact
@@ -26,5 +30,7 @@ with
 
 
 select 
-    *
+    get_latest_flow_uuid.*,
+    contacts.user_type_from_google_sheets 
 from get_latest_flow_uuid
+left join contacts using (contact_phone)
