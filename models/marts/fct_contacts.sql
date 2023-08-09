2324e79bd0b9eabc972b7994ad7c14f7d6d9fcac
Vatsal Gosalia <vatsalgosalia8@gmail.com>
2023-08-09 15:52:07 +0530

    user department from contact fields, edited the other file

--- models/marts/fct_contacts.sql
+++ models/marts/fct_contacts.sql
@@ -5,12 +5,15 @@ with
 
     received_message_metrics as (select * from {{ ref('int_inbound_message_metrics') }}),
 
-    latest_flow as (select * from {{ ref('int_latest_flow_for_user') }})
+    latest_flow as (select * from {{ ref('int_latest_flow_for_user') }}),
+
+    latest_flow_before_ten_days as (select * {{ ref('int_message_not_received') }})
 
 select
-    *,
+    *
 from
     contacts
     left join AWS_metrics using (google_sheet_id)
     left join latest_flow using (contact_phone)
     left join received_message_metrics using (contact_phone)
+    left join latest_flow_before_ten_days using (contact_phone)
