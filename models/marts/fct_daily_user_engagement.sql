2672ef5abb245efd2c68a10ca660aa4f5c69d97d
Vatsal Gosalia <vatsalgosalia8@gmail.com>
2023-09-26 18:52:10 +0530

    change int daily user engagement, remove flow uuid. did unique count of flows

--- models/marts/fct_daily_user_engagement.sql
+++ models/marts/fct_daily_user_engagement.sql
@@ -16,21 +16,21 @@ with
         select
             contact_phone,
             date_day,
-            flow_uuid_started,
-            case when date_started is not null then 'Yes' else "No"
-            end as flow_started,
-            flow_uuid_opted_in,
+            flows_started,
+            case when flows_started is not null then 'Yes' else "No"
+            end as flows_started_bool,
+            flows_opted_in,
             case
-                when date_opted_in is not null then 'Yes' else "No"
-            end as flow_opted_in,
-            flow_uuid_completed,
+                when flows_opted_in is not null then 'Yes' else "No"
+            end as flows_opted_in_bool,
+            flows_completed,
             case
-                when date_completed is not null then 'Yes' else "No"
-            end as flow_completed,
-            flow_uuid_succeeded,
+                when flows_completed is not null then 'Yes' else "No"
+            end as flows_completed_bool,
+            flows_succeeded,
             case
-                when date_succeeded is not null then 'Yes' else "No"
-            end as flow_succeeded
+                when flows_succeeded is not null then 'Yes' else "No"
+            end as flows_succeeded_bool
         from daily_user_engagement
     ),
 
