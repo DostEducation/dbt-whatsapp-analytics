afc00dd130ce3d32a2a2535c1d098b168d832311
Vatsal Gosalia <vatsalgosalia8@gmail.com>
2023-09-26 13:44:49 +0530

    add registration date column

--- models/marts/fct_daily_user_engagement.sql
+++ models/marts/fct_daily_user_engagement.sql
@@ -1,5 +1,26 @@
 with daily_user_engagement as (select * from {{ ref('int_daily_user_engagement') }}),
     contacts as (select * from {{ ref('fct_contacts') }}),
+    messages as (select * from {{ ref('fct_messages') }}),
+    add_row_number_for_messages as (
+        select
+            *,
+            row_number() over (
+                partition by contact_phone, flow_uuid order by message_inserted_at desc
+            ) as row_number
+        from messages
+        where message_inserted_at >= '2023-09-01' and node_label = 'pf01n01'
+    ),
+
+    select_latest_record_for_messages as (
+        select * from add_row_number_for_messages where row_number = 1
+    ),
+    find_user_registration_date as(
+        select
+            contact_phone,
+            flow_uuid,
+            cast(message_inserted_at as date) as registration_date
+        from select_latest_record_for_messages
+    ),
 
     change_date_bool as (
         select
@@ -22,7 +43,10 @@ select
     programme,
     city,
     experience_type,
-    sign_up_status
+    sign_up_status,
+    flow_uuid as flow_uuid_registration,
+    registration_date
 from change_date_bool
 left join contacts using (contact_phone)
-
+left join find_user_registration_date on find_user_registration_date.contact_phone = change_date_bool.contact_phone
+                                        and find_user_registration_date.registration_date = change_date_bool.date_day
